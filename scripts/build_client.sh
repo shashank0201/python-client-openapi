e010f454f8089b4cb4b4fae1fdd125e9dcbc0232
shashank0201 <shashankcs11@gmail.com>
2020-12-22 14:01:52 +0530

    Update build_client.sh

--- scripts/build_client.sh
+++ scripts/build_client.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_PATH=`dirname $0`
 FULL_PATH=`realpath ${SCRIPT_PATH}/..`
-docker run --rm -v ${FULL_PATH}:/stoplight-client openapitools/openapi-generator-cli generate -i /stoplight-client/openapi.yml -g python -o /stoplight-client/ --additional-properties=packageName=tech.client.config
+docker run --rm -v ${FULL_PATH}:/my-python-client openapitools/openapi-generator-cli generate -i /my-python-client/openapi.yml -g python -o /stoplight-client/ --additional-properties=packageName=tech.client.config
 sudo chown -R $USER $FULL_PATH 
